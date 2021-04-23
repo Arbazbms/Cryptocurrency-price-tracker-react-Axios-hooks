5b860a7a319d1a82414d46fe942f9ce377dfe8cd
= <ArbazAhmed.cs19@bmsce.ac.in>
2021-04-23 16:40:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
 
   useEffect(()=>{
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false')
     .then((res) => {
       setCoins(res.data);
       console.log(res.data)
@@ -21,9 +21,10 @@ function App() {
     setSearch(e.target.value);
   }
 
-  const filteredCoins = coins.filter(coin => {
+  const filteredCoins = coins.filter(coin => 
     coin.name.toLowerCase().includes(search.toLowerCase())
-  })
+  )
+  
 
   return (
     <div className="coin-app">
@@ -38,10 +39,12 @@ function App() {
           <Coin
           key={coin.id}
           name={coin.name}
-          image = {coin.image}
-          symbol={coin.symbol}
-          volume={coin.market_cap}
           price={coin.current_price}
+          symbol={coin.symbol}
+          marketcap={coin.total_volume}
+          volume={coin.market_cap}
+          image={coin.image}
+          priceChange={coin.price_change_percentage_24h}
           />  
         );      
    
